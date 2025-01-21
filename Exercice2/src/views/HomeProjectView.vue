a3c08e102348a8f03338926cbdb2a9da80e3c087
glouglouu <buiche.mathieu@gmail.com>
2025-01-21 01:33:28 +0100

    cleaning and spliting exercices in two folders 2.0

---
+++ Exercice2/src/views/HomeProjectView.vue
@@ -0,0 +1,137 @@
+<script lang="ts" setup>
+import HomeCard from '@/components/HomeCard.vue';
+import cards from '../cards.json'
+</script>
+<template>
+<div class="container project-box">
+        <section class="dashboard">
+          <div class="project">
+            <h4>Your Projects</h4>
+            <div class="project-grid">
+              <HomeCard v-for="card in cards" :key="card.id" :card="card"/>
+              </div>
+            </div>
+          <section class="side">
+            <div class="announcement">
+              <h4>Announcements</h4>
+              <div class="background-box">
+                <div class="anounce">
+                  <h5>Site Maintenance</h5>
+                  <p>
+                    Lorem ipsum dolor sit amet, officia excepteur ex fugiat
+                    reprehenderit enim labore culpa sint ad nisi Lorem pariatur
+                    mollit ex esse exercitation amet. Nisi anim cupidatat
+                    excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem
+                    est aliquip amet voluptate voluptate dolor minim nulla est
+                    proident. Nostrud officia pariatur ut officia. Sit irure
+                    elit esse ea nulla sunt ex occaecat reprehenderit commodo
+                    officia dolor Lorem duis laboris cupidatat officia
+                    voluptate. Culpa proident adipisicing id nulla nisi laboris
+                    ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit
+                    commodo ex non excepteur duis sunt velit enim. Voluptate
+                    laboris sint cupidatat ullamco ut ea consectetur et est
+                    culpa et culpa duis.
+                  </p>
+                </div>
+                <hr />
+                <div class="anounce">
+                  <h5>Community Share Day</h5>
+                  <p>
+                    Lorem ipsum dolor sit amet, officia excepteur ex fugiat
+                    reprehenderit enim labore culpa sint ad nisi Lorem pariatur
+                    mollit ex esse exercitation amet. Nisi anim cupidatat
+                    excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem
+                    est aliquip amet voluptate voluptate dolor minim nulla est
+                    proident. Nostrud officia pariatur ut officia. Sit irure
+                    elit esse ea nulla sunt ex occaecat reprehenderit commodo
+                    officia dolor Lorem duis laboris cupidatat officia
+                    voluptate. Culpa proident adipisicing id nulla nisi laboris
+                    ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit
+                    commodo ex non excepteur duis sunt velit enim. Voluptate
+                    laboris sint cupidatat ullamco ut ea consectetur et est
+                    culpa et culpa duis.
+                  </p>
+                </div>
+                <hr />
+                <div class="anounce">
+                  <h5>Updated Privacy Policy</h5>
+                  <p>
+                    Lorem ipsum dolor sit amet, officia excepteur ex fugiat
+                    reprehenderit enim labore culpa sint ad nisi Lorem pariatur
+                    mollit ex esse exercitation amet. Nisi anim cupidatat
+                    excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem
+                    est aliquip amet voluptate voluptate dolor minim nulla est
+                    proident. Nostrud officia pariatur ut officia. Sit irure
+                    elit esse ea nulla sunt ex occaecat reprehenderit commodo
+                    officia dolor Lorem duis laboris cupidatat officia
+                    voluptate. Culpa proident adipisicing id nulla nisi laboris
+                    ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit
+                    commodo ex non excepteur duis sunt velit enim. Voluptate
+                    laboris sint cupidatat ullamco ut ea consectetur et est
+                    culpa et culpa duis.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div>
+              <h4>Trending</h4>
+              <div class="background-box trending">
+                <div class="trend">
+                  <figure>
+                    <img
+                      class="avatar"
+                      src="https://robohash.org/irwi32.png?set=set4"
+                      alt="avatar"
+                    />
+                  </figure>
+                  <div class="trend-details">
+                    <h6>@tegan</h6>
+                    <p>World Peace Builder</p>
+                  </div>
+                </div>
+                <div class="trend">
+                  <figure>
+                    <img
+                      class="avatar"
+                      src="https://robohash.org/ijrwi32.png?set=set4"
+                      alt="avatar"
+                    />
+                  </figure>
+                  <div class="trend-details">
+                    <h6>@morgan</h6>
+                    <p>Super cool Project</p>
+                  </div>
+                </div>
+                <div class="trend">
+                  <figure>
+                    <img
+                      class="avatar"
+                      src="https://robohash.org/irw2.png?set=set4"
+                      alt="avatar"
+                    />
+                  </figure>
+                  <div class="trend-details">
+                    <h6>@kindall</h6>
+                    <p>Life Changing App</p>
+                  </div>
+                </div>
+                <div class="trend">
+                  <figure>
+                    <img
+                      class="avatar"
+                      src="https://robohash.org/ireqwe2.png?set=set4"
+                      alt="avatar"
+                    />
+                  </figure>
+                  <div class="trend-details">
+                    <h6>@alex</h6>
+                    <p>No Traffic Maker</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        </section>
+      </div>
+</template>
+<style></style>
---
+++ Exercice2/src/views/NextuRegistrationView.vue
@@ -0,0 +1,66 @@
+<script lang="ts" setup>
+import {reactive} from "vue"
+import type {Student} from "@/models/student"
+
+const student:Student = reactive({
+  name:'',
+  lastName:'',
+  theme:''
+});
+const themes = reactive([
+  "Machine Learning",
+  "LLM",
+  "NoSql"
+])
+const onSubmit = ()=>{
+  console.log(student.name)
+  console.log(student.lastName);
+  console.log(student.theme)
+}
+</script>
+<template>
+  <form @submit.prevent="onSubmit">
+    <label for="fname">First Name</label>
+    <input type="text" id="fname" name="firstname" placeholder="Your name.." v-model="student.name"/>
+    <label for="lname">Last Name</label>
+    <input type="text" id="lname" name="lastname" placeholder="Your last name.." v-model="student.lastName"/>
+    <label for="theme">Filière</label>
+    <select id="theme" name="theme" v-model="student.theme">
+      <option v-for="(item,i) in themes" :key="i" :value="item">{{item}}</option>
+    </select>
+    <button type="submit">Enregistrer</button>
+  </form>
+</template>
+<style scoped>
+input[type='text'],
+select {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+
+input[type='submit'] {
+  width: 100%;
+  background-color: #4caf50;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+input[type='submit']:hover {
+  background-color: #45a049;
+}
+
+div {
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px;
+}
+</style>
